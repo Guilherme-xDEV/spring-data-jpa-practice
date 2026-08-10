e83f119475192b1ec7b9460fd9718c4343fb0eff
Guilherme-xDEV <guilhermegm2020k@gmail.com>
2026-08-09 21:56:42 -0300

    chore: initial commit

---
+++ src/test/java/com/guilherme/example/spring_data_jpa_practice/SpringDataJpaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.example.spring_data_jpa_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
